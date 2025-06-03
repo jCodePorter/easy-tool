7f78bd6cfc1532ecb5c16cf1586f65fd897d3a41
augrain <248366210@qq.com>
2025-06-03 23:17:15 +0800

    feat:字符串拆分

--- src/main/java/cn/augrain/easy/tool/core/StringUtils.java
+++ src/main/java/cn/augrain/easy/tool/core/StringUtils.java
@@ -1,5 +1,14 @@
 package cn.augrain.easy.tool.core;
 
+import cn.augrain.easy.tool.consts.StrConst;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * 字符串
  *
@@ -253,5 +262,55 @@ public class StringUtils {
             return new String[0];
         }
         return str.split(regex);
+    }
+
+    /**
+     * 切分字符串(分隔符默认逗号)
+     *
+     * @param str 被切分的字符串
+     * @return 分割后的数据列表
+     */
+    public static List<String> splitList(String str) {
+        return splitTo(str, String::valueOf);
+    }
+
+    /**
+     * 切分字符串
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符
+     * @return 分割后的数据列表
+     */
+    public static List<String> splitList(String str, String separator) {
+        return splitTo(str, separator, String::valueOf);
+    }
+
+    /**
+     * 切分字符串自定义转换(分隔符默认逗号)
+     *
+     * @param str    被切分的字符串
+     * @param mapper 自定义转换
+     * @return 分割后的数据列表
+     */
+    public static <T> List<T> splitTo(String str, Function<? super Object, T> mapper) {
+        return splitTo(str, StrConst.STR_COMMA, mapper);
+    }
+
+    /**
+     * 切分字符串自定义转换
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符
+     * @param mapper    自定义转换
+     * @return 分割后的数据列表
+     */
+    public static <T> List<T> splitTo(String str, String separator, Function<? super Object, T> mapper) {
+        if (isBlank(str)) {
+            return new ArrayList<>(0);
+        }
+        return Arrays.stream(str.split(separator))
+                .filter(Objects::nonNull)
+                .map(mapper)
+                .collect(Collectors.toList());
     }
 }
